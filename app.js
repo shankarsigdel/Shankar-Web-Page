84072a11b19c1f2f95078a9faec5676bd07dbda8
Shankar <shankarsigdel26@gmail.com>
2021-06-26 22:33:39 -0400

    JavaScript Added

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-// let button = document.querySelector("button");
 
-// button.addEventListener("click", ()=>{
-//     alert("Hello");
-// })
+
+
+let images = document.querySelectorAll(".project-image");
+let popup = document.querySelector(".popup-wrapper");
+let popupImage = document.querySelector("#popup-img");
+
+
+images.forEach((image) => {
+    image.addEventListener("click", (event) => {
+        console.log(event.target);
+        popupImage.src = event.target.src;
+        popup.style.display = "block";
+    })
+})
+
+popup.addEventListener("click", (event) => {
+    if (event.target === popupImage ) {
+        return;
+    }
+     popup.style.display = "none";
+})
